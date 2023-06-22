5fb868788856e60de68653f40e429411bfca3156
tanhao <th101292@163.com>
2023-06-22 11:03:56 +0800

    allmlp lora

--- scripts/baseline/train2.sh
+++ scripts/baseline/train2.sh
@@ -1,15 +1,40 @@
-# fgvc_aircraft oxford_pets stanford_cars dtd eurosat oxford_flowers
-for DATASET in fgvc_aircraft
-do
-    for r in 2 4 8
-    do
-        for alpha in 0.4 0.6 1. 2.
-        do
-            CUDA_VISIBLE_DEVICES=1 python train_wandb_iter_val.py \
-            --dataset-config-file /home/tanhao/Baseline/configs/datasets/${DATASET}.yaml \
-            --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
-            DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA ${alpha} MODEL.LORA.RANK ${r} \
-            OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5
-        done
-    done
-done
+CUDA_VISIBLE_DEVICES=0 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 0.4 MODEL.LORA.RANK 2 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=1 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 0.6 MODEL.LORA.RANK 2 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=2 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 1. MODEL.LORA.RANK 2 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=3 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 2. MODEL.LORA.RANK 2 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=4 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 0.4 MODEL.LORA.RANK 4 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=5 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 0.6 MODEL.LORA.RANK 4 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=6 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 1. MODEL.LORA.RANK 4 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
+CUDA_VISIBLE_DEVICES=7 WANDB_API_KEY=2a0ff77c64888b3bd539c7873069809fbfeb6059 WANDB_MODE=offline python /home/lijun07/code/CAPT/train_wandb_iter_val.py \
+    --dataset-config-file /home/tanhao/Baseline/configs/datasets/fgvc_aircraft.yaml \
+    --config-file /home/tanhao/Baseline/configs/trainers/Baseline_lora/vit_b16.yaml \
+    DATASET.NUM_SHOTS 16 MODEL.LORA.ALPHA 2. MODEL.LORA.RANK 4 \
+    OPTIM.MAX_ITER 19200 OPTIM.LR 5e-5 & \
