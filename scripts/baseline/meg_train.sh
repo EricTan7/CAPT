a89cc9fc86c5c14785acaa5d4c00df10051400dd
Bazinga699 <1402083730@qq.com>
2023-06-22 11:23:37 +0800

    reduce numworker

--- scripts/baseline/meg_train.sh
+++ scripts/baseline/meg_train.sh
@@ -1,10 +1,10 @@
 # for DATASET in caltech101 dtd fgvc_aircraft food101 eurosat ucf101 oxford_flowers oxford_pets stanford_cars imagenet sun397
-rlaunch --gpu=8 --cpu=32 --memory=128000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 1 & \
+rlaunch --gpu=8 --cpu=50 --memory=200000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 1 & \
 
-rlaunch --gpu=8 --cpu=32 --memory=128000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 2 & \
+rlaunch --gpu=8 --cpu=50 --memory=200000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 2 & \
 
-rlaunch --gpu=8 --cpu=32 --memory=128000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 4 & \
+rlaunch --gpu=8 --cpu=50 --memory=200000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 4 & \
 
-rlaunch --gpu=8 --cpu=32 --memory=128000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 8 & \
+rlaunch --gpu=8 --cpu=50 --memory=200000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 8 & \
 
-rlaunch --gpu=8 --cpu=32 --memory=128000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 16 & \
+rlaunch --gpu=8 --cpu=50 --memory=200000 --positive-tags=2080ti --max-wait-duration=24h -- bash /home/lijun07/code/CAPT/scripts/baseline/train_8datasets.sh 16 & \
