cfce78f22edfd8af3a1ca8620b06acffdb7fa4b6
Bazinga699 <1402083730@qq.com>
2023-06-24 22:14:26 +0800

    1

---
+++ scripts/baseline/meg_train_part3-1.sh
@@ -0,0 +1,9 @@
+bash /home/lijun07/code/CAPT/scripts/baseline/train2.sh eurosat 16 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train2.sh eurosat 8 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train2.sh eurosat 4 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train2.sh eurosat 2 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train2.sh eurosat 1 & \
