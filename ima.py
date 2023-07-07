2ad4a5d38eeb7e8a74f0d3550ead72373dd6d9e4
lijun07 <lijun07@megvii.com>
2023-07-07 15:59:48 +0800

    missing some img in imagenet

--- ima.py
+++ ima.py
@@ -4,16 +4,16 @@ import json
 
 path2id = {}
 name2path = {}
-nori_path = '/data/datasets/imagenet/imagenet.val.nori.list'
+nori_path = '/data/datasets/imagenet/imagenet.train.nori.list'
 
 with open('/data/datasets/imagenet/path2id.json','r') as f:
     path2id = json.load(f)
 
-with open('/data/datasets/imagenet/preprocessed.pkl', "rb") as f:
-    preprocessed = pickle.load(f)
-    test = preprocessed['test']
-    for item in test:
-        name2path[ item['impath'].split('/')[-1] ] = os.path.join(*item['impath'].split('/')[-3:])
+# with open('/data/datasets/imagenet/preprocessed.pkl', "rb") as f:
+#     preprocessed = pickle.load(f)
+#     test = preprocessed['test']
+#     for item in test:
+#         name2path[ item['impath'].split('/')[-1] ] = os.path.join(*item['impath'].split('/')[-3:])
 
 with open(nori_path) as g:
     l = g.readline()
@@ -24,19 +24,19 @@ with open(nori_path) as g:
         path2id[path] = ls[0]
         l = g.readline()
 
-with open('/data/datasets/imagenet/path2id.json','w') as f:
-    json.dump(path2id,f)
+# with open('/data/datasets/imagenet/path2id.json','w') as f:
+#     json.dump(path2id,f)
 
-# with open('/data/datasets/imagenet/wval_split_fewshot_caption/shot_16-seed_1.pkl', "rb") as f:
-#     preprocessed = pickle.load(f)
-#     for item in preprocessed['train']:
-#         path = item['impath']
-#         if path.split('/')[-3] != 'train':
-#             print(path)
-#     for item in preprocessed['val']:
-#         path = item['impath']
-#         if path.split('/')[-3] != 'train':
-#             print(path)
+with open('/data/datasets/imagenet/wval_split_fewshot_caption/shot_16-seed_1.pkl', "rb") as f:
+    preprocessed = pickle.load(f)
+    for item in preprocessed['train']:
+        path = item['impath']
+        if path.split('/')[-3] != 'train':
+            print(path)
+    for item in preprocessed['val']:
+        path = item['impath']
+        if path.split('/')[-3] != 'train':
+            print(path)
     
 #     a = 1
 
