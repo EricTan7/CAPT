32de8a02f750a068550a2e594915e3df71f08b60
tanhao <th101292@163.com>
2023-07-10 10:31:00 +0800

    abl_vitl_text_sup, 5~6.sh, 45 in total

---
+++ scripts/baseline/meg_train_part6.sh
@@ -0,0 +1,19 @@
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh stanford_cars 16 38400 5e-5 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh stanford_cars 8 38400 5e-5 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh stanford_cars 4 38400 5e-5 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh stanford_cars 2 38400 5e-5 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh stanford_cars 1 38400 5e-5 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh sun397 16 12800 1e-4 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh sun397 8 12800 1e-4 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh sun397 4 12800 1e-4 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh sun397 2 12800 1e-4 & \
+
+bash /home/lijun07/code/CAPT/scripts/baseline/train4.sh sun397 1 12800 1e-4 & \
